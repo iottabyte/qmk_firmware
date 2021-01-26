ed992ffffec8a42d1770dfa0340640df9fd8801b
gigabyte <synthgyod@gmail.com>
2021-01-26 17:19:44 -0500

    started working on macros

--- keyboards/nullbitsco/tidbit/keymaps/via/keymap.c
+++ keyboards/nullbitsco/tidbit/keymaps/via/keymap.c
@@ -4,84 +4,171 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Base Layout:
+ * ,-----------------------.
+ * |     |  F1 |  << |  >> | 
+ * |------+------+------+--|
+ * |     |  F2 |  F5 | F12 | 
+ * |------+------+------+--|
+ * |  7  |  8  |  9  |  0  | 
+ * |------+------+------+--|
+ * |  4  |  5  |  6  |     |
+ * |------+------+------+--|
+ * |  1  |  2  |  3  |     |    
+ * `-----------------------`
  */
 
 #include QMK_KEYBOARD_H
+#include "action_layer.h"
 #include "remote_kb.h"
 #include "bitc_led.h"
 
+
 #define _BASE     0
-#define _VIA1     1
-#define _VIA2     2
-#define _VIA3     3
+#define _MISC     1
+#define _PHSP     2
+#define _STUP     3
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT(
-        KC_F1,   KC_F2, KC_F3, \
-  KC_7, KC_8,     KC_9, KC_PSLS, \
-  KC_4, KC_5,     KC_6, KC_PAST, \
-  KC_1, KC_2,     KC_3, KC_PMNS, \
-  KC_0, KC_DOT, KC_ENT, KC_PPLS  \
-  ),
-
-  [_VIA1] = LAYOUT(
-           KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
-  ),
-
-  [_VIA2] = LAYOUT(
-           KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
-  ),
-
-  [_VIA3] = LAYOUT(
-           KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
-  ),
+enum custom_keycodes {
+  PROG = SAFE_RANGE,
+  SEL,
+  DESEL,
+  LIQ,
+  TRFM,
+  N_LYR,
+  UNDO,
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  process_record_remote_kb(keycode, record);
-  return true;
-}
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // Base layer (numpad & useful keys)
+  [_BASE] = LAYOUT(
+           KC_F1,    KC_MPRV, KC_MNXT, \
+  MO(1), KC_F2,  KC_F5,      KC_F12, \
+  KC_KP_7, KC_KP_8,  KC_KP_9,  KC_DEL, \
+  KC_KP_4, KC_KP_5,  KC_KP_6,  KC_LANG4, \
+  KC_KP_1,   KC_KP_2,  KC_KP_3, KC_KP_0 \
+  ),
+  
+  // Function layer (misc navigation and media controls)
+  [_MISC] = LAYOUT(
+           KC_WFAV, KC_MPLY, KC_MUTE, \
+    KC_NO, KC_NO, RGB_MOD, KC_PSCR, \
+    KC_HOME, KC_UP, KC_PGUP, RGB_TOG, \
+    KC_LEFT, KC_NO, KC_RGHT, RGB_M_SW, \
+    KC_END,  KC_DOWN, KC_PGDN, RGB_M_P \
+  ),
 
+  // Photoshop layer (photoshop shortcuts)
+  [_PHSP] = LAYOUT(
+            MO(1), N_LYR, LIQ, \
+    KC_NO, KC_CUT, KC_COPY, KC_PSTE, \
+    KC_LBRC, KC_RBRC, TRFM, SEL, \
+    KC_L, KC_SPC, KC_I, DESEL, \
+    KC_E,  KC_N, KC_B, UNDO \
+  ),
+
+  // Setup layer (configuration keys)
+  [_STUP] = LAYOUT(
+            PROG, KC_NO, KC_NO, \
+  KC_NO, KC_NO, KC_NO, KC_TRNS, \
+  KC_MYCM, KC_NO, KC_NO, KC_TRNS, \
+  KC_NO, KC_NO, KC_NO, KC_NO, \
+  KC_PWR, KC_EJCT, KC_NO, KC_SLEP  \
+  ),
+
+};
+
+// Initialization function 2: Happens midway through the firmware’s startup process.
 void matrix_init_user(void) {
   matrix_init_remote_kb();
-  set_bitc_LED(LED_OFF);
 }
 
 void matrix_scan_user(void) {
   matrix_scan_remote_kb();
 }
 
+// custom keys and macros
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  process_record_remote_kb(keycode, record);
+
+  switch(keycode) {
+    case PROG:
+      if (record->event.pressed) {
+        set_bitc_LED(LED_DIM);
+        rgblight_disable_noeeprom();
+        bootloader_jump(); //jump to bootloader
+      }
+    break;
+
+    case UNDO:
+      if (record->event.pressed) {
+          SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_X)SS_UP(X_LCRTL))
+      } 
+      break;
+
+    case SEL:
+
+
+    case DESEL:
+
+
+    case LIQ:
+
+
+    case TRFM:
+
+
+    case N_LYR:
+
+    default:
+    break;
+  }
+  return true;
+}
+
+// encoder layer select
+uint8_t selected_layer = 0;
 void encoder_update_user(uint8_t index, bool clockwise) {
-  if (clockwise) {
-    tap_code(KC_VOLU);
-  } else {
-    tap_code(KC_VOLD);
+  switch (index) {
+    case 0:
+      if (!clockwise && selected_layer  < 10) {
+        selected_layer ++;
+      } else if (clockwise && selected_layer  > 0) {
+        selected_layer --;
+      }
+      layer_clear();
+      layer_on(selected_layer);
+ 
   }  
 }
 
+// underglow change based on layer (WS2812B)
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _MISC: // light green
+        rgblight_setrgb (0x95,  0xEF, 0x8E);
+        break;
+    case _PHSP: // soft blue
+        rgblight_setrgb (0x9E,  0xB5, 0xE0);
+        break;
+    case _STUP: // goldenrod
+        rgblight_setrgb (0xFB,  0xD4, 0x26);
+        break;
+    default: //  for any other layers, or the default layer (pink)
+        rgblight_setrgb (0xEC,  0x55, 0x93);
+        break;
+    }
+  return state;
+}
+
+// LED control on micro controller
 void led_set_kb(uint8_t usb_led) {
-  if (usb_led & (1<<USB_LED_NUM_LOCK))
+  if (usb_led & (1<<USB_LED_CAPS_LOCK))
     set_bitc_LED(LED_DIM);
   else
     set_bitc_LED(LED_OFF);
 }
+
+
